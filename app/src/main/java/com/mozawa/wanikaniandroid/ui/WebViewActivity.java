1473b80cf063d430936f9dc7aa4321885ab07195
Momo Ozawa <momo.ozawa@pluscitizen.com>
2016-08-12 10:21:37 -0700

    Update styling of the cards

--- app/src/main/java/com/mozawa/wanikaniandroid/ui/WebViewActivity.java
+++ app/src/main/java/com/mozawa/wanikaniandroid/ui/WebViewActivity.java
@@ -1,5 +1,7 @@
 package com.mozawa.wanikaniandroid.ui;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -12,27 +14,43 @@ import com.mozawa.wanikaniandroid.ui.base.BaseActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ReviewActivity extends BaseActivity {
+public class WebViewActivity extends BaseActivity {
+
+    public static final String EXTRA_TOOLBAR_TITLE = "WebViewActivity.EXTRA_TOOLBAR_TITLE";
+    public static final String EXTRA_URL = "WebViewActivity.EXTRA_URL";
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.webView)
     WebView webView;
 
+    String toolbarTitle;
     String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_review);
+        setContentView(R.layout.activity_webview);
         ButterKnife.bind(this);
 
-        setSupportActionBar(toolbar);
+        // fetch extras from previous activity
+        getExtras();
 
-        // fetch url from previous activity
-        fetchUrl();
+        toolbar.setTitle(toolbarTitle);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         setCustomWebViewSettings();
         webView.loadUrl(url);
+
+        Log.d("momo", toolbarTitle);
+    }
+
+    public static Intent getStartIntent(Context context, String toolbarTitle, String url) {
+        Intent intent = new Intent(context, WebViewActivity.class);
+        intent.putExtra(EXTRA_TOOLBAR_TITLE, toolbarTitle);
+        intent.putExtra(EXTRA_URL, url);
+        return intent;
     }
 
     private void setCustomWebViewSettings() {
@@ -40,17 +58,17 @@ public class ReviewActivity extends BaseActivity {
         webView.setWebViewClient(new WebViewClient() { // allows use in-situ
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                Log.d("URL", url);
                 view.loadUrl(url);
                 return false;
             }
         });
     }
 
-    private void fetchUrl() {
+    private void getExtras() {
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            url = extras.getString("URL");
+            toolbarTitle = extras.getString(EXTRA_TOOLBAR_TITLE);
+            url = extras.getString(EXTRA_URL);
         }
     }
 }
