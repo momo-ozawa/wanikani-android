3efecb2bb9cb0ed91178ca6cd75464813845f8b4
Momo Ozawa <mozawa92@gmail.com>
2016-08-23 11:56:19 -0400

    Create Vocabulary model

---
+++ app/src/main/java/com/mozawa/wanikaniandroid/data/model/Vocabulary.java
@@ -0,0 +1,52 @@
+package com.mozawa.wanikaniandroid.data.model;
+
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Vocabulary {
+
+    /*
+    {
+      "user_information": {
+        "username": "momo-ozawa",
+        "gravatar": "26c11682771a8d300ac1b6b225d3e52b",
+        "level": 1,
+        "title": "Guppies",
+        "about": "",
+        "website": null,
+        "twitter": null,
+        "topics_count": 0,
+        "posts_count": 0,
+        "creation_date": 1462411044,
+        "vacation_date": null
+      },
+      "requested_information": [
+        {
+          "character": "二",
+          "kana": "に",
+          "meaning": "two",
+          "level": 1,
+          "user_specific": null
+        }
+      ]
+    }
+     */
+
+    @SerializedName("user_information")
+    public UserInformation userInformation;
+    @SerializedName("requested_information")
+    public List<VocabularyInformation> vocabularyInformationList;
+
+    public static class VocabularyInformation {
+
+        public String character;
+        public String kana;
+        public String meaning;
+        public int level;
+        @SerializedName("user_specific")
+        public UserSpecific userSpecific;
+
+    }
+}
