1bd067c2b74939e55d42870d73b8744d3cd5d796
Momo Ozawa <mozawa92@gmail.com>
2016-08-23 16:23:53 -0400

    Create custom RatioRelativeLayout

---
+++ app/src/main/java/com/mozawa/wanikaniandroid/ui/widgets/RatioRelativeLayout.java
@@ -0,0 +1,39 @@
+package com.mozawa.wanikaniandroid.ui.widgets;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class RatioRelativeLayout extends RelativeLayout {
+
+    /**
+     * A RelativeLayout where we can specify the height and width ratio.
+     *
+     * For reference:
+     *   http://stackoverflow.com/a/29320794\
+     */
+
+    private static final double WIDTH_RATIO = 4;
+    private static final double HEIGHT_RATIO = 5;
+
+    public RatioRelativeLayout(Context context) {
+        super(context);
+    }
+
+    public RatioRelativeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RatioRelativeLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSize = (int) (HEIGHT_RATIO / WIDTH_RATIO * widthSize);
+        int newHeightSpec = MeasureSpec.makeMeasureSpec(heightSize, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, newHeightSpec);
+    }
+}
