a9e68f75f487bea10cd3e0202e452d2cc8e25a13
Momo Ozawa <mozawa92@gmail.com>
2016-11-20 14:53:43 +0900

    Play around with constraint layout for kanji detail activity

--- app/src/main/java/com/mozawa/wanikaniandroid/ui/kanjidetail/KanjiDetailActivity.java
+++ app/src/main/java/com/mozawa/wanikaniandroid/ui/kanjidetail/KanjiDetailActivity.java
@@ -1,7 +1,11 @@
 package com.mozawa.wanikaniandroid.ui.kanjidetail;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.mozawa.wanikaniandroid.R;
 
@@ -11,5 +15,17 @@ public class KanjiDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kanji_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
 }
